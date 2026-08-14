911ddd3d3185b6c56696d79f8dd8bcf7b89099c2
kechiput11 <apizblokm@gmail.com>
2026-08-14 14:10:31 +0800

    Remove temporary scripts to fix build

--- read_excel.ts
+++
@@ -1,5 +0,0 @@
-import * as xlsx from "xlsx";
-
-const filePath = "C:\\Users\\mhafi\\Downloads\\REKAP DATABASE.xlsx";
-const workbook = xlsx.readFile(filePath);
-console.log("Sheet Names:", workbook.SheetNames);
